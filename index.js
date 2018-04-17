2784016a0cf6d430e405c7a6783d6ef92a50249a
Kenny Yang <kennyyang19@gmail.com>
2018-04-17 04:47:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -70,11 +70,6 @@ function removeFromCart(item) {
 
 
 function placeOrder(cardNumber) {
-  // write your code here
-}
-
-
-function placeOrder(cardNumber) {
 
   if (!cardNumber){
   return `Sorry, we don't have a credit card on file for you.`
